985d62bb9807e680754a7aaf8bee42864425987b
Никита Ивашин <ivashin.nekit.2000@mail.ru>
2023-09-26 15:12:52 +0300

    Готов к просмотру

---
+++ index.js
@@ -0,0 +1,57 @@
+let burger = document.querySelector(".burger");
+let menu = document.querySelector(".header__nav");
+let menuLinks = menu.querySelectorAll(".nav__link");
+console.log(menuLinks)
+
+
+burger.addEventListener("click",
+    
+    function () {
+        burger.classList.toggle("burger--active");
+
+        menu.classList.toggle("header__nav--active");
+        
+        document.body.classList.toggle("stop-scroll");
+    }   
+)
+
+menuLinks.forEach(function (el) {
+    el.addEventListener("click", function () {
+        
+        burger.classList.remove("burger--active");
+
+        menu.classList.remove("header__nav--active");
+
+        document.body.classList.remove("stop-scroll")
+    })
+})
+
+
+const btnMore = document.querySelector('.articles-more');
+
+const articlesItems = document.querySelectorAll('.articles-item');
+
+btnMore.addEventListener('click', () => {
+    articlesItems.forEach(el => { el.classList.add('articles-item--visible') });
+    btnMore.closest('.articles-center').classList.add('articles-center--hidden');
+
+})
+
+// Swiper
+const swiper = new Swiper('.swiper', {
+    direction: 'vertical',
+    loop: true,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
